674a0f83465eb214d2ef4669a8f0cdc1c2a47c00
wuyulong <l>
2019-02-16 21:49:00 +0800

    first commit

---
+++ src/main/java/com/star/javatuples/JavatuplesApplication.java
@@ -0,0 +1,14 @@
+package com.star.javatuples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatuplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavatuplesApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/star/javatuples/JavatuplesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.star.javatuples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavatuplesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
